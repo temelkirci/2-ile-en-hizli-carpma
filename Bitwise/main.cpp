77e9781cd49dd154ac145e54032c70c1b7c8fd92
Temel <kircitemel@gmail.com>
2018-01-15 22:14:36 +0300

    15 january

--- Bitwise/main.cpp
+++ Bitwise/main.cpp
@@ -14,10 +14,25 @@ int divide_by_pow(int number , int power) // power kadar biti sağa kaydırarak 
 	return number>>power;
 }
 
+int Mult(int a)
+{
+	_asm
+	{
+		mov eax, a
+		add eax, a
+	}
+}
+
+int mult(int a)
+{
+	return a*2;
+}
+
 int main()
 {
 	auto sayi1 = 1600000;
 	auto sayi2 = 1600000;
+	auto sayi3 = 1600000;
 	const int size = 1000;
 
 	high_resolution_clock::time_point t1 = high_resolution_clock::now(); // saymaya başla t1 = başlangıç zamanı
@@ -30,28 +45,37 @@ int main()
 	cout<<endl;
 	high_resolution_clock::time_point t2 = high_resolution_clock::now(); // saymayı bitir t2 = bitiş zamanı
 	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-	cout<<"Total Time : "<<time_span.count()<<" seconds"<<endl<<endl;
+	cout<<"mult_by_pow() Total Time : "<<time_span.count()<<" seconds"<<endl<<endl;
 
 	
 	high_resolution_clock::time_point t3 = high_resolution_clock::now();// saymaya başla t3 = başlangıç zamanı
 	
 	for(int i=0 ; i<size ; i++)
 	{
-		cout<<sayi2*2;
+		cout<<mult(sayi2);
 	}
 	
 	cout<<endl;
-	high_resolution_clock::time_point t4 = high_resolution_clock::now();// saymayı bitir t4 = bitiş zamanı
+	high_resolution_clock::time_point t4 = high_resolution_clock::now();// saymayı bitir t5 = bitiş zamanı
 	duration<double> time_span1 = duration_cast<duration<double>>(t4 - t3);
-	cout<<"Total Time : "<<time_span1.count()<<" seconds"<<endl<<endl;
+	cout<<"sayi*2 Total Time : "<<time_span1.count()<<" seconds"<<endl<<endl;
 
-	if(time_span1.count() > time_span.count())
-		cout<<"sola kaydirarak carpma daha hizli : "<<time_span.count()<<endl<<endl;
-	else
-		cout<<"x2 ile carpma daha hizli : "<<time_span1.count()<<endl<<endl;
+
+	high_resolution_clock::time_point t5 = high_resolution_clock::now();// saymaya başla t6 = başlangıç zamanı
+	
+	for(int i=0 ; i<size ; i++)
+	{
+		cout<<Mult(sayi3);
+	}
+	
+	cout<<endl;
+	high_resolution_clock::time_point t6 = high_resolution_clock::now();// saymayı bitir t4 = bitiş zamanı
+	duration<double> time_span2 = duration_cast<duration<double>>(t6 - t5);
+	cout<<"Total Time : "<<time_span2.count()<<" seconds"<<endl<<endl;
 
 	cout<<"bitwise mult : "<<time_span.count()<<endl;
 	cout<<"normal mult : "<<time_span1.count()<<endl;
+	cout<<"assembly mult : "<<time_span2.count()<<endl;
 
 	system("pause");
 	return 0;
